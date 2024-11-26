0d0ea60aead5adc4895ee840107ccb0112e0bbe7
raullcordova <rc925e@gmail.com>
2024-11-26 09:45:43 -0600

    6th commit

--- app.py
+++ app.py
@@ -104,4 +104,6 @@ def download_file(filename):
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Use the dynamic port provided by Render or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
